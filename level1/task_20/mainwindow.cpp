a82f72dc54d01a6390002da0678c658cb9004afa
Donara Tarkhanyan <138888396+donaratarkhanyan@users.noreply.github.com>
2024-02-28 23:25:50 +0400

    Add files via upload

---
+++ level1/task_20/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    int32_t a = ui ->label->text().toInt();
+    a++;
+    ui ->label->setText(QString::number(a));
+}
+
